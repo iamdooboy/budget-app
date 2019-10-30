6fc8bdb04f1730647599475dc19fba24820832f8
Duy Le <asianinvasion.le@gmail.com>
2019-10-30 11:51:47 -0400

    setting up event listeners

--- app.js
+++ app.js
@@ -25,8 +25,25 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 	
-	document.querySelector('add__btn').addEventListener('click', function() {
-		console.log('button was clicked');
+	var ctlrAddItem = function() {
+		//1. get the filed input data
+		
+		//2. add the item to the budget controller
+		
+		//3. add the item to the UI
+		
+		//4. calculate the budget
+		
+		//5. display the budget on the UI
+	}
+	
+	document.querySelector('add__btn').addEventListener('click', ctlrAddItem);
+	
+	//pressing enter
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode === 13 || event.which === 13) {
+		   ctlrAddItem();
+		}
 	});
 	
 })(budgetController, UIController);
